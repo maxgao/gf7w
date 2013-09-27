74b0ab680f52fda182e94ce338a47a399020c385
gaofeng <gaofeng@gf.(none)>
2013-09-27 15:55:17 +0800

    socket 应用test.py

---
+++ socket/test.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+
+import socket
+
+HOST='baidu.com'
+PORT=80
+BUFFER=4096
+
+
+sock=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+sock.connect((HOST,PORT))
+message='GET / HTTP/1.1\r\nHost: %s \r\n\r\n'  % HOST
+
+try:
+	sock.send(message)
+except socket.error:
+	print "send failed"
+
+recv=sock.recv(BUFFER)
+
+print('[tcpServer said]: %s' % recv)
+
+sock.close()
