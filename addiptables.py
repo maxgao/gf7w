7034ba453e66da725b41cb35213820f8a3449df4
gaofeng <gaofeng@gf.(none)>
2013-11-05 22:25:27 +0800

    change log addr

--- addiptables.py
+++ addiptables.py
@@ -1,10 +1,11 @@
-#!/usr/bin/python
+#!/usr/bin/python 
+#-*-coding:utf8-*-
 #file里的文件不能为空，随便写个
 import os
 import time
 
 def add_iptables(ip):
-    f = open('/root/iptables','a+')
+    f = open('/home/gaofeng/iptables','a+')
     for row in f.readlines():
         if ip == row[:-1]:
             break
@@ -15,4 +16,6 @@ def add_iptables(ip):
     f.close()
 
 
-add_iptables('1.1.1.2')
+if __name__=="__main__":
+
+    add_iptables('1.1.1.2')
