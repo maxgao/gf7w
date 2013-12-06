306b16ee900adf3d053d28389a285c356d7878c2
gaofeng <maxgao@dnspod.com>
2013-12-06 17:27:04 +0800

    aaa

--- push_mysql_res.sh
+++ push_mysql_res.sh
@@ -5,5 +5,5 @@ now=`date +%Y-%m-%d`
 echo $now
 
 
-/usr/bin/mysql -pnaizhao1234 cacti -e "select * from user_auth_realm into outfile '/tmp/$now'"
+/usr/bin/mysql -pna cacti -e "select * from user_auth_realm into outfile '/tmp/$now'"
 
